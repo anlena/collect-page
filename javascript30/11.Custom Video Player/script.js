2618623ae308c319c7604d933a5bde3aa3fc8874
anlena <superiron_man@163.com>
2017-04-24 17:46:40 +0800

    hi

--- javascript30/11.Custom Video Player/script.js
+++ javascript30/11.Custom Video Player/script.js
@@ -7,7 +7,7 @@ let skip = document.querySelectorAll('[data-skip]');
 let filled = document.querySelector('.progress__filled');
 let progressBar = document.querySelector('.progress');
 
-// 实现函数
+// 
 function videoplay(e) {
     // if(video.paused){
     //     video.play();
@@ -18,26 +18,36 @@ function videoplay(e) {
     video[method]();
 }
 
+
+// 播放按钮状态显示函数
 function handleToggle() {
     let icon = video.paused ? '►' : '❚ ❚';
     toggle.textContent = icon;
 }
 
+
+// 音量大小和播放速度控制函数
 function handlePlayerSlider(e) {
     video[e.target.name] = e.target.value;
 }
 
+
+//快进快退函数
 function handleSkip(e) {
     let skiptime = parseFloat(this.dataset.skip);
     video.currentTime += skiptime;
 }
 
+
+// 拖拽进度条定点观看的函数
 function handlefilled(e) {
     let pice = (e.offsetX / progressBar.offsetWidth) * video.duration;
     // let pice = (e.offsetX / progressBar.clientWidth) * video.duration;
     video.currentTime = pice;
 }
 
+
+//进度条随播放时间而显示的函数
 function filledUpdate() {
     let portion = parseFloat(video.currentTime / video.duration) * 100;
     filled.style.flexBasis = `${portion}%`;
